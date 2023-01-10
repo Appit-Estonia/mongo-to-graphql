226846617c2998556b31c5f62cf603b3862960b9
Arnold Sanglepp <arnold@frillice.com>
2023-01-10 17:09:35 +0200

    Update filters logic

--- src/resolverLogic/helpers.ts
+++ src/resolverLogic/helpers.ts
@@ -1,4 +1,6 @@
 import { Document } from "mongoose";
+import { ObjectId } from "..";
+import { BaseFilterParams } from "../context/types/request";
 import { Sorting } from "../context/types/setup";
 import { TResolver } from "./types";
 
@@ -43,3 +45,53 @@ export const populateProps = (pathKey: string) => {
     isArray: pathKey.startsWith("["),
   }
 }
+
+
+const filterComparisonMap = {
+  equals: "eq",
+  like: "regex",
+  not: "ne",
+  greaterThan: "gt",
+  lessThan: "lt",
+  greaterOrEquals: "gte",
+  lessOrEquals: "lte",
+  in: "in",
+  inLike: "in",
+  notIn: "nin",
+  numberEquals: "eq",
+  numberNot: "ne",
+  numberGreaterThan: "gt",
+  numberLessThan: "lt",
+  numberGreaterOrEquals: "gte",
+  numberLessOrEquals: "lte",
+  numberIn: "in",
+  numberNotIn: "nin",
+  booleanEquals: "eq",
+}
+
+const isValidObjectId = (id: string) => {
+  return ObjectId.isValid(id) && (String)(new ObjectId(id)) === id;
+}
+
+const getLikeRegex = (value: string) => new RegExp(".*" + value + ".*", "i");
+const getValue = (value: any) => isValidObjectId(value) ? new ObjectId(value) : value;
+
+export const getFilterComparison = (filter: BaseFilterParams) => {
+  // Get comparison key from filter object. As there shoudn't be more filter clauses per filter then by default first comparison clause will be used
+  const {fieldKey, ...rest} = filter;
+  const comparisonKey = Object.keys(rest)?.[0] as keyof Omit<BaseFilterParams, 'fieldKey'>;
+
+  if (!comparisonKey) return undefined;
+
+  // Handle cases where value is not always string or number. 
+  switch (comparisonKey) {
+    case "between":
+      return { [fieldKey]: { "$gte": getValue(filter.between?.from), "$lte": getValue(filter.between?.to) } }
+    case "numberBetween":
+      return { [fieldKey]: { "$gte": getValue(filter.numberBetween?.from), "$lte": getValue(filter.numberBetween?.to) } }
+    case "inLike":
+      return {[fieldKey]: { "$in": getValue(filter.inLike?.map(i => getLikeRegex(i)))}}
+    default:
+      return {[fieldKey]: {[`$${filterComparisonMap[comparisonKey]}`]: getValue(filter[comparisonKey])}}
+  }
+};
